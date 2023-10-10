ffed084cae66609a9b7a04bc8b4e1db0ce55e329
Tommy Grullón Contreras <tommycontreras34@gmail.com>
2023-10-10 11:49:54 -0400

    Added information to the app controller to manage request

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,8 +5,18 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
+  @Get('/greeting')
+  async getHello() {
     return this.appService.getHello();
+  }
+
+  @Get('/greeting-async')
+  async getHelloAsync() {
+    return this.appService.getHelloAsync();
+  }
+
+  @Get('/publish-event')
+  async publishEvent() {
+    this.appService.publishEvent();
   }
 }
