6130c531fdc01e7b69e9429ff793d6c5d08ab8a4
Tommy Grullón Contreras <tommycontreras34@gmail.com>
2023-10-10 11:47:25 -0400

    Added functions to retrieve information from the microservice

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(@Inject('GREETING_SERVICE') private client: ClientProxy) {}
+
+  async getHello() {
+    return this.client.send({ cmd: 'greeting' }, 'Progressive Coder');
+  }
+
+  async getHelloAsync() {
+    return this.client.send({ cmd: 'greeting-async' }, 'Progressive Coder');
+  }
+
+  async publishEvent() {
+    this.client.emit('book-created', { 'bookName': 'The Way Of Kings', 'author': 'Brandon Sanderson' });
   }
 }
